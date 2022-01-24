2f192d6e2874d1f200aa3049dfebb36f8cacdf39
karishma <karishmacarterjohnson@gmail.com>
2022-01-24 13:17:36 -0800

    Initial Commit

---
+++ double-checked/double_checkedApp.swift
@@ -0,0 +1,17 @@
+//
+//  double_checkedApp.swift
+//  double-checked
+//
+//  Created by Karishma Johnson on 1/24/22.
+//
+
+import SwiftUI
+
+@main
+struct double_checkedApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
