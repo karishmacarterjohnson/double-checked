aba57a35f3a5ab445ea5d411f12b4eb7511322d4
karishma <karishmacarterjohnson@gmail.com>
2022-01-27 15:52:18 -0800

    updated colors

--- double-checked/Components/ProgressBar.swift
+++ double-checked/Components/ProgressBar.swift
@@ -16,10 +16,10 @@ struct ProgressBar: View {
             ZStack(alignment: .leading) {
                 Rectangle().frame(width: geometry.size.width , height: geometry.size.height)
                     .opacity(0.3)
-                    .foregroundColor(Color(UIColor.systemTeal))
+                    .foregroundColor(Color(UIColor.systemPink))
                 withAnimation {
                 Rectangle().frame(width: min(CGFloat(self.value)*geometry.size.width, geometry.size.width), height: geometry.size.height)
-                    .foregroundColor(Color(UIColor.systemBlue))
+                    .foregroundColor(Color(UIColor.systemOrange))
                 }
             }.cornerRadius(45.0)
         }
