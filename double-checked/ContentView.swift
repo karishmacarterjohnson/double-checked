5a87b99edf349915eb9f0e65500adb5d5e3cf2c7
karishma <karishmacarterjohnson@gmail.com>
2022-01-27 13:14:36 -0800

    refactored progress bar to its own file

--- double-checked/ContentView.swift
+++ double-checked/ContentView.swift
@@ -7,23 +7,6 @@
 
 import SwiftUI
 
-struct ProgressBar: View {
-    @Binding var value: Float
-    
-    var body: some View {
-        GeometryReader { geometry in
-            ZStack(alignment: .leading) {
-                Rectangle().frame(width: geometry.size.width , height: geometry.size.height)
-                    .opacity(0.3)
-                    .foregroundColor(Color(UIColor.systemTeal))
-                withAnimation {
-                Rectangle().frame(width: min(CGFloat(self.value)*geometry.size.width, geometry.size.width), height: geometry.size.height)
-                    .foregroundColor(Color(UIColor.systemBlue))
-                }
-            }.cornerRadius(45.0)
-        }
-    }
-}
 
 struct ContentView: View {
     @Environment(\.managedObjectContext) private var viewContext
