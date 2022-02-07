df5f2c9ba365119cf4730297bba24fc647fdc40f
karishma <karishmacarterjohnson@gmail.com>
2022-02-06 17:31:04 -0800

    added error handling for url input

--- double-checked/Components/ActivityLinks.swift
+++ double-checked/Components/ActivityLinks.swift
@@ -14,31 +14,45 @@ struct ActivityLinks: View {
     @StateObject var activity: Activity
     @State var itemLink: String = ""
     @State var showTextField: Bool = false
+    @State private var showingAlert = false
+
     
     var body: some View {
         ScrollView(.horizontal) {
             
             HStack {
                 if showTextField {
-                    TextField("add link", text: $itemLink)
-                        .textFieldStyle(.roundedBorder)
-                    Button(action: addLinkItem) {
-                        Label("", systemImage:"plus")
+                    HStack {
+                        TextField("add link", text: $itemLink)
+                            .textFieldStyle(.roundedBorder)
+                        Button(action: addLinkItem) {
+                            Label("", systemImage:"plus")
+                        }
                     }
+                    .alert(isPresented: $showingAlert) {
+                    Alert(
+                        title: Text("Error"),
+                        message: Text("Invalid url. Try adding 'http://' to the beginning if you think this is a mistake."),
+                        dismissButton: .default(Text("ok"))
+                    )
+                    }
+                    .frame(maxWidth: 160)
+                    //.frame(maxWidth: UIScreen.main.bounds.size.width)
                 } else {
                     Button(action: {showTextField.toggle()}) {
-                    Image(systemName: "plus")
-                        .font(.largeTitle)
+                        Image(systemName: "plus")
+                            .font(.largeTitle)
                     }
                 }
                 ForEach(activity.linkItemsArray) { linkitem in
                     
                     Link(destination: URL(string: linkitem.unwrappedLink)!) {
                         HStack {
-                            VStack {
+                            VStack(alignment: .leading) {
                                 Text(linkitem.unwrappedTitle)
                                 Text(linkitem.unwrappedLink)
-                            }
+                                    .font(.caption)
+                            }.frame(maxWidth: 160)
                             Image(systemName: "link.circle.fill")
                                 .font(.largeTitle)
                         }
@@ -60,23 +74,25 @@ struct ActivityLinks: View {
                 do {
                     let contents = try String(contentsOf: url)
                     let range = NSRange(location: 0, length: contents.utf16.count)
-
+                    
                     let regex = try! NSRegularExpression(pattern: "<title>.*?</title>")
                     let match = regex.matches(in: contents, range: range)
                     let matchArray = match.map {
                         String(contents[Range($0.range, in: contents)!])
                     }
                     newLinkItem.title = String(matchArray[0].dropFirst(7).dropLast(8))
+                    activity.addToLinkitems(newLinkItem)
+                    
+                    PersistenceController.shared.saveContext()
+                    itemLink = ""
+                    showTextField.toggle()
                     
                 } catch {
-                    newLinkItem.title = itemLink
+                    showingAlert = true
+
                 }
                 
-                activity.addToLinkitems(newLinkItem)
                 
-                PersistenceController.shared.saveContext()
-                itemLink = ""
-                showTextField.toggle()
             } else {
                 // alert bad link
                 print("bad url!")
