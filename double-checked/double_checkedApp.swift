446edfd8ddd3e8e7d26c30504233ba535357a672
karishma <karishmacarterjohnson@gmail.com>
2022-02-09 09:50:00 -0800

    added app delegate & custom url

--- double-checked/double_checkedApp.swift
+++ double-checked/double_checkedApp.swift
@@ -9,12 +9,27 @@ import SwiftUI
 
 @main
 struct double_checkedApp: App {
+    @Environment(\.managedObjectContext) private var viewContext
+    @UIApplicationDelegateAdaptor(AppDelegate.self) var appDelegate
     let persistenceController = PersistenceController.shared
     
     var body: some Scene {
         WindowGroup {
             ContentView()
                 .environment(\.managedObjectContext, persistenceController.viewContext)
+//                .onOpenURL(perform: <#T##(URL) -> ()#>)
         }
     }
+    
+}
+
+final class AppDelegate: NSObject, UIApplicationDelegate {
+    func application(
+        _ application: UIApplication,
+        didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey : Any]? = nil) -> Bool {
+            // do something
+            return true
+    }
+
+    // Other delegate methods here...
 }
