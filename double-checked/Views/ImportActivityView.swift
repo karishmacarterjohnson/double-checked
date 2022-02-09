3c7e697ce80f56694382fbe35499789eaa3abe8a
karishma <karishmacarterjohnson@gmail.com>
2022-02-09 14:26:13 -0800

    url goes to preview page

--- double-checked/Views/ImportActivityView.swift
+++ double-checked/Views/ImportActivityView.swift
@@ -10,21 +10,21 @@ import SwiftUI
 struct ImportActivityView: View {
     @Environment(\.managedObjectContext) private var viewContext
     
-    @StateObject var activity: Activity
-
+    //@StateObject var activity: Activity
+    var url: String
+//    @StateObject private var activity: Activity
+    
     var body: some View {
-//        ShareActivity(activity: activity)
-//        ImportActivity()
+        Text("arrived!")
         VStack {
-            ProgressBar(value: progressValue(activity: activity)).frame(height:10).padding(.horizontal)
-
-            ActivityLinks(activity: activity)
             
+            ActivityLinks(activity: importActivityAsObject())
+
             List {
                 ForEach(groupItems(), id:\.self.0){ activityName, items in
                     Section(header: Text(activityName ?? "")){
                         ForEach(items) { item in
-                            Checked(activity:activity, item: item)
+                            Checked(activity: importActivityAsObject(), item: item)
                         }
                     }
                 }
@@ -35,8 +35,35 @@ struct ImportActivityView: View {
             Button(action: cancelSaveActivity) {
                 Label("Close", systemImage: "")
             }
-        }.navigationBarTitle(activity.unwrappedTitle)
+        }.navigationBarTitle(importActivityAsObject().unwrappedTitle)
     }
+    
+    private func importActivityAsObject() -> Activity {
+        let b64 = String(url.dropFirst("doublechecked://".count))
+        let data = Data(base64Encoded: b64)
+        let b64Decoded = String(data: data!, encoding: .utf8) // string of json
+        let jsonData = (b64Decoded?.data(using:.utf8))! // string
+        let attempt = try! JSONDecoder().decode(ActivityShared.self, from: jsonData)  // ActivityShared struct
+//        return "nice, v"
+        let importedActivity = Activity(context: viewContext)
+        importedActivity.title = attempt.title
+        importedActivity.date = attempt.date
+        for i in attempt.items {
+            let itemCopy = Item(context: viewContext)
+            itemCopy.title = i[0]
+            itemCopy.activityTitle = i[1]
+            importedActivity.addToItems(itemCopy)
+        }
+        for l in attempt.linkItems {
+            let linkItemCopy = LinkItem(context: viewContext)
+            linkItemCopy.title = l[0]
+            linkItemCopy.link = l[1]
+            importedActivity.addToLinkitems(linkItemCopy)
+        }
+        return importedActivity
+
+    }
+    
     
     private func cancelSaveActivity() {
         // go back to main view??
@@ -48,25 +75,26 @@ struct ImportActivityView: View {
         PersistenceController.shared.saveContext()
     }
     
-    private func toggleCheck(item: Item) {
-        withAnimation {
-            var ct: Int = 0
-            let newItem = Item(context: viewContext)
-            for i in activity.itemsArray {
-                if ct == 0 && item.title == i.title && item.activityTitle == i.activityTitle {
-                    newItem.title = item.title
-                    newItem.activityTitle = item.activityTitle
-                    newItem.check = !item.check
-                    activity.addToItems(newItem)
-                    viewContext.delete(i)
-                    ct += 1
-                    PersistenceController.shared.saveContext()
-                }
-            }
-        }
-    }
+//    private func toggleCheck(item: Item) {
+//        withAnimation {
+//            var ct: Int = 0
+//            let newItem = Item(context: viewContext)
+//            for i in activity.itemsArray {
+//                if ct == 0 && item.title == i.title && item.activityTitle == i.activityTitle {
+//                    newItem.title = item.title
+//                    newItem.activityTitle = item.activityTitle
+//                    newItem.check = !item.check
+//                    activity.addToItems(newItem)
+//                    viewContext.delete(i)
+//                    ct += 1
+//                    PersistenceController.shared.saveContext()
+//                }
+//            }
+//        }
+//    }
     
     private func groupItems() -> [(String?,[Item])] {
+        let activity = importActivityAsObject()
         var items: Dictionary = Dictionary(grouping: activity.itemsArray, by: {$0.activityTitle})
         let current: [Item]? = items[activity.unwrappedTitle]
         items.removeValue(forKey: activity.unwrappedTitle)
@@ -83,41 +111,41 @@ struct ImportActivityView: View {
         return listItems
         
     }
-    
-    
-    private func progressValue(activity: Activity) -> Float {
-        let total: Int = activity.itemsArray.count
-        var checkedCount: Int = 0
-        for x in activity.itemsArray {
-            if x.check {
-                checkedCount += 1
-            }
-        }
-        return Float(Double(checkedCount) / Double(total))
-    }
-    
-    struct ReadActivityView_Previews: PreviewProvider {
-        static var previews: some View {
-            let viewContext = PersistenceController.preview.container.viewContext
-            let newActivity = Activity(context: viewContext)
-            newActivity.title = "visit gf"
-            newActivity.date = Date()
-            
-            let item1 = Item(context: viewContext)
-            item1.title = "1 item"
-            item1.activityTitle = "group"
-            
-            newActivity.addToItems(item1)
-            
-            @FetchRequest(
-                sortDescriptors: [NSSortDescriptor(keyPath: \Activity.date, ascending: true)], // https://www.donnywals.com/fetching-objects-from-core-data-in-a-swiftui-project/
-                animation: .default)
-            var activityArray: FetchedResults<Activity>
-            
-            return ReadActivityView(activity: newActivity, activityArray: activityArray)
-                .environment(\.managedObjectContext,
-                              PersistenceController.preview.container.viewContext)
-        }
-    }
+//    
+//    
+//    private func progressValue(activity: Activity) -> Float {
+//        let total: Int = activity.itemsArray.count
+//        var checkedCount: Int = 0
+//        for x in activity.itemsArray {
+//            if x.check {
+//                checkedCount += 1
+//            }
+//        }
+//        return Float(Double(checkedCount) / Double(total))
+//    }
+//    
+//    struct ReadActivityView_Previews: PreviewProvider {
+//        static var previews: some View {
+//            let viewContext = PersistenceController.preview.container.viewContext
+//            let newActivity = Activity(context: viewContext)
+//            newActivity.title = "visit gf"
+//            newActivity.date = Date()
+//            
+//            let item1 = Item(context: viewContext)
+//            item1.title = "1 item"
+//            item1.activityTitle = "group"
+//            
+//            newActivity.addToItems(item1)
+//            
+//            @FetchRequest(
+//                sortDescriptors: [NSSortDescriptor(keyPath: \Activity.date, ascending: true)], // https://www.donnywals.com/fetching-objects-from-core-data-in-a-swiftui-project/
+//                animation: .default)
+//            var activityArray: FetchedResults<Activity>
+//            
+//            return ReadActivityView(activity: newActivity, activityArray: activityArray)
+//                .environment(\.managedObjectContext,
+//                              PersistenceController.preview.container.viewContext)
+//        }
+//    }
     
 }
--- double-checked/double_checkedApp.swift
+++ double-checked/double_checkedApp.swift
@@ -12,24 +12,48 @@ struct double_checkedApp: App {
     @Environment(\.managedObjectContext) private var viewContext
     @UIApplicationDelegateAdaptor(AppDelegate.self) var appDelegate
     let persistenceController = PersistenceController.shared
-    
+    @State public var previewActivity: Bool = false
+//    @State public var importedActivity: Activity
+    @State public var importedActivity: String = ""
     var body: some Scene {
         WindowGroup {
+            if previewActivity {
+                ImportActivityView(url: importedActivity)
+                //"doublechecked://eyJ0aXRsZSI6IkJhZyIsIml0ZW1zIjpbWyJTbmFjayIsIkJhZyJdXSwibGlua0l0ZW1zIjpbXX0="
+                    .environment(\.managedObjectContext, persistenceController.viewContext)
+            } else {
             ContentView()
                 .environment(\.managedObjectContext, persistenceController.viewContext)
-//                .onOpenURL(perform: <#T##(URL) -> ()#>)
+                .onOpenURL(perform: {url in
+                    previewActivity = true
+                    importedActivity = url.absoluteString
+                })
+            }
         }
     }
-    
 }
 
 final class AppDelegate: NSObject, UIApplicationDelegate {
     func application(
         _ application: UIApplication,
         didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey : Any]? = nil) -> Bool {
-            // do something
             return true
     }
-
     // Other delegate methods here...
 }
+
+
+//
+//    .onOpenURL(perform: {url in
+//        let data = Data(base64Encoded: url.absoluteString)
+//        let b64Decoded = String(data: data!, encoding: .utf8) // string of json
+//        let jsonData = (b64Decoded?.data(using:.utf8))! // string
+//        let attempt = try! JSONDecoder().decode(ActivityShared.self, from: jsonData)  // ActivityShared struct
+////        return "nice, v"
+//        importedActivity = Activity(context: viewContext)
+//        importedActivity.title = attempt.title
+//        importedActivity.date = attempt.date
+//        previewActivity = true
+//        // use attempt to create an activity,
+//
+//    })
